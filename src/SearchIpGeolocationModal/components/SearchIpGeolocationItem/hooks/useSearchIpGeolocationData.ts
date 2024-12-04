2a2ac88aa59933ab577d72ceea6aecb8c36f7418
Yegor Kozlov <yegovik@gmail.com>
2024-12-04 19:08:01 +0100

    Move api interaction on hook level

--- src/SearchIpGeolocationModal/components/SearchIpGeolocationItem/hooks/useSearchIpGeolocationData.ts
+++ src/SearchIpGeolocationModal/components/SearchIpGeolocationItem/hooks/useSearchIpGeolocationData.ts
@@ -1,10 +1,18 @@
 import { useCallback, useState } from "react";
-import { searchGeolocationData } from "../../../../ip.service";
 
 export type GeolocationData = {
   countryFlag: string;
   timeZone: string;
 };
+
+type GeolocationAPIResponse = {
+  country_flag: string;
+  time_zone: {
+    name: string;
+  }
+};
+
+const GEOLOCATION_API_KEY = process.env.REACT_APP_IP_GEOLOCATION_API_KEY;
 
 export const useSearchIpGeolocationData = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -13,7 +21,14 @@ export const useSearchIpGeolocationData = () => {
     setIsLoading(true);
 
     try {
-      const result = await searchGeolocationData(ip);
+      const searchParams = new URLSearchParams({
+        apiKey: GEOLOCATION_API_KEY || '',
+        ip,
+        fields: ['country_flag', 'time_zone'].join(',')
+      });
+      const response = await fetch(`https://api.ipgeolocation.io/ipgeo?${searchParams.toString()}`);
+      const result: GeolocationAPIResponse = await response.json();
+
       return {
         countryFlag: result.country_flag,
         timeZone: result.time_zone.name,
